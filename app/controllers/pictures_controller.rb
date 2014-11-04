117fe497c640975c9f12c34c0eed7f94a900de5c
daynaclowe <daynalowe@Dayna-Lowes-MacBook-3.local>
2014-11-04 17:28:00 -0500

    Moving @pictures in pictures_controller.rb into database

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,43 +1,17 @@
 class PicturesController < ApplicationController
 	def index
-		@pictures = [ 
-			{
-				:title => "The old church on the coast of White Sea",
-				:artist => "Sergey Ershov",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-			},
-			{
-				:title => "Into the Poppies",
-				:artist => "John Wilhelm",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-			},
-			{
-				:title => "Sea Power",
-				:artist => "Stephen Scullion",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-			}
-		]
+		@pictures = Picture.all
 	end
 
 	def show
-		@pictures = [ 
-			{
-				:title => "The old church on the coast of White Sea",
-				:artist => "Sergey Ershov",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/house.jpg"
-			},
-			{
-				:title => "Into the Poppies",
-				:artist => "John Wilhelm",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/girl.jpg"
-			},
-			{
-				:title => "Sea Power",
-				:artist => "Stephen Scullion",
-				:url => "http://bitmakerlabs.s3.amazonaws.com/photogur/wave.jpg"
-			}
-		]
-		@picture = @pictures[params[:id].to_i]
+		@picture = Picture.find(params[:id])
+	end
+
+	def new
+	end
+
+	def create
+		render :text => "Saving a picture. URL:#{params[:url]}, Title: #{params[:title]}, Artist: #{[:artist]}"
 	end
 
 end
